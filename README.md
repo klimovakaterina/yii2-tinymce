4e93d7b18f363adabdd8ea99a1dad251e2a9ee7a
Klimova Katerina <klimovakatya1@gmail.com>
2019-09-10 19:32:52 +0300

    fixed errors

---
+++ README.md
@@ -0,0 +1,9 @@
+Установка
+
+composer require klimovakaterina/yii2-tinymce "*@dev"
+
+Использование
+
+use klimovakaterina\EditorWidget\EditorWidget;
+
+echo EditorWidget::widget(['field' => 'basic-example']); //basic-example - id textarea
--- src/EditorWidgetAsset.php
+++ src/EditorWidgetAsset.php
@@ -6,7 +6,7 @@ use yii\web\AssetBundle;
 
 class EditorWidgetAsset extends AssetBundle
 {
-    public $sourcePath = "@app/components/EditorWidget/assets";
+    public $sourcePath = __DIR__ . '/assets';
 
     public $js = [
         'js/tinymce.min.js',
